ed962d4a847b0c8c12327db24d6b85c46c44bdbf
Levraichacal <sjp.bardot@gmail.com>
2017-10-07 22:22:20 +0200

    Add GNU Licence
    Adding the GNU licence to the files

--- Usefull-Librairies/ArduinoUno/gpio.adb
+++ Usefull-Librairies/ArduinoUno/gpio.adb
@@ -1,3 +1,34 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                             L V C COMPONENTS                             --
+--                                                                          --
+--                                G P I O                                   --
+--                                                                          --
+--                                B o d y                                   --
+--                                                                          --
+--    Copyright (C) 2017 Bardot Sébastien                                   --
+--    More information on http://lvcetada.e-monsite.com/ or by mail on      --
+--    lvcetada@gmail.com                                                    --
+--                                                                          --
+--    This program is free software: you can redistribute it and/or modify  --
+--    it under the terms of the GNU General Public License as published by  --
+--    the Free Software Foundation, either version 3 of the License, or     --
+--    (at your option) any later version.                                   --
+--                                                                          --
+--    This program is distributed in the hope that it will be useful,       --
+--    but WITHOUT ANY WARRANTY; without even the implied warranty of        --
+--    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         --
+--    GNU General Public License for more details.                          --
+--                                                                          --
+--    You should have received a copy of the GNU General Public License     --
+--    along with this program.  If not, see <http://www.gnu.org/licenses/>. --
+--                                                                          --
+--                                                                          --
+--    This package and its children are based on AVR-Ada Library.           --
+--                                                                          --
+--                                                                          --
+------------------------------------------------------------------------------
+
 package body GPIO is
 
    -- La valeur du port : Haut = true, Bas = false;
