ca55870f7da15612327c4a73f268ffc78cb4d876
LeVraiChacal <sjp.bardot@gmail.com>
2020-11-22 16:48:40 +0100

    Clean repo and fix interrupt uno

--- Old_repo/6_8 - Interruption Materielle sur Uno/button.adb
+++ Old_repo/6_8 - Interruption Materielle sur Uno/button.adb
@@ -14,21 +14,23 @@ package body Button is
    procedure Button_Interrupt;
    pragma Machine_Attribute (Entity         => Button_Interrupt,
                              Attribute_Name => "signal");
-   pragma Export (C, Button_Interrupt, MCU.Sig_PCINT2_String);
+   pragma Export (C, Button_Interrupt, MCU.Sig_PCINT1_String);
 
    procedure Button_Interrupt is
    begin
       MCU.PCMSK1_Bits (MCU.PCINT8_Bit) := False;
-      MCU.PORTB_Bits (7) := not MCU.PORTB_Bits (7);
+      MCU.PORTB_Bits (5) := not MCU.PORTB_Bits (5);
       MCU.PCMSK1_Bits (MCU.PCINT8_Bit) := True;
    end Button_Interrupt;
 
    procedure Run is
    begin
+      AVR.Interrupts.Disable;
       MCU.DDRC_Bits (0) := DD_Input; -- A0 
-      MCU.DDRB_Bits (7) := DD_Output; -- D13
+      MCU.PORTC_Bits(0) := True;
+      MCU.DDRB_Bits (5) := DD_Output; -- D13
 
-      MCU.PORTB_Bits (7) := True;
+      MCU.PORTB_Bits (5) := True;
 
       MCU.PCICR_Bits (MCU.PCIE1_Bit) := True;
       MCU.PCMSK1_Bits (MCU.PCINT8_Bit) := True;
