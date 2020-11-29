d2e9da4af9933accd380a1fd71b302afaaaf1f83
LeVraiChacal <sjp.bardot@gmail.com>
2020-11-29 21:57:38 +0100

    clean

--- Old_repo/Projet_Eclairage_Maison_Poupee/lvc-buttons-interrupts.ads
+++
@@ -1,9 +0,0 @@
-------------------------------------------------------
--- The package contains all the interruptions when a
--- button inside the house is press
-
-package LVC.BUTTONS.INTERRUPTS is
-
-   procedure Init;
-
-end LVC.BUTTONS.INTERRUPTS;
